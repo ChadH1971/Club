f2630842250acc02b494a562010c7e1b3ed17e56
Chad Hidalgo <hidalgochad@gmail.com>
2022-10-07 05:20:28 -0700

    added guild name and member loops

--- pythonProject/bot.py
+++ pythonProject/bot.py
@@ -6,6 +6,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
+my_guild = os.getenv("DISCORD_GUILD")
 
 intents = discord.Intents.default()
 client = discord.Client(intents=intents)
@@ -13,6 +14,13 @@ client = discord.Client(intents=intents)
 
 @client.event
 async def on_ready():
-    print(f'{client.user} has connected to Discord!')
+    for guild in client.guilds:
+        if guild.name == my_guild:
+            break
+
+    print(f'{client.user} has connected to {guild.name} (id: {guild.id})')
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}')
 
 client.run(token)
